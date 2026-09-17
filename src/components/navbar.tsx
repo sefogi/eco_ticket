a45dcb635057b8877d39a8a6f9f00ffbcc4d979a
sefogi <sebast18@gmail.com>
2026-09-18 01:53:53 +0200

    se cambio el tamaño de la fuente del navabar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg border-bottom border-success border-5 bg-white">
       <div className="container-fluid px-4">
         <a className="navbar-brand d-flex align-items-center fw-bold text-success ms-lg-5" href="/">
-          <img src="logo.png" alt="EcoTíquet" height="52" className="me-2" />
+          <img src="logo.png" alt="EcoTíquet" height="60" className="me-2" />
         </a>
 
         <button
@@ -21,30 +21,30 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarEcoTicket">
           <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link text-secondary fw-semibold" href="/">
+              <a className="nav-link text-secondary fw-semibold fs-5 " href="/">
                 Inicio
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-secondary fw-semibold" href="/Eventos">
+              <a className="nav-link text-secondary fw-semibold fs-5" href="/Eventos">
                 Eventos
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-secondary fw-semibold" href="/MisTickets">
+              <a className="nav-link text-secondary fw-semibold fs-5" href="/MisTickets">
                 Mis Tickets
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-secondary fw-semibold" href="/Soporte">
+              <a className="nav-link text-secondary fw-semibold fs-5" href="/Soporte">
                 Soporte y Ayuda
               </a>
             </li>
           </ul>
 
           <div className="d-flex align-items-center gap-2 me-lg-5">
-            <a className="fw-semibold text-success"href="/Login">Mi Cuenta</a>
-            <i className="bi bi-person-circle fs-4 text-success"></i>
+            <a className="fw-semibold text-success fs-5"href="/Login">Mi Cuenta</a>
+            <i className="bi bi-person-circle fs-2 text-success"></i>
           </div>
         </div>
       </div>
